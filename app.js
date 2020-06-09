78e59d8f91a3f106a564a2d227fc4fe7f6a89222
STEVANUS BILLY <stevanusbilly00@mail.com>
2020-06-09 22:37:37 +0700

    tambah cek

--- app.js
+++ app.js
@@ -226,6 +226,7 @@ app.post("/cekBayar",async(req,res)=>{
             // TODO set transaction status on your databaase to 'success'
             pool.getConnection(function(err,conn){
                 console.log("berhasil bayar settlement")
+                console.log(transaction_id)
                 conn.query(`update user set type=1 where transaction_id='${transaction_id}'`,function(error,result){
                     return res.status(200).send({
                         "status":"200",
